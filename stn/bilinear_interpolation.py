081614962efa134f9cc8647a0f24652a4e34507d
Pascal Bustamante <pascalbustamante46@gmail.com>
2024-02-08 15:23:12 -0600

    interpolation

--- stn/bilinear_interpolation.py
+++ stn/bilinear_interpolation.py
@@ -1,19 +1,28 @@
 import numpy as np
+import torch
 import cv2
 
 def get_pixel_values(img, x, y):
     """
-    Get raw pixel values at coord (x,y)
-    Input:
-        img: CV2.image (B, H, W, C)
-        x, y: tensors of shape (B*H*W)
-    
-    Returns:
-        output: Tensor (B, H, W, C)
+    Utility function to get pixel value for coordinate
+    vectors x and y from a  4D tensor image.
+    Input
+    -----
+    - img: tensor of shape (B, C, H, W)
+    - x: flattened tensor of shape (B*H*W)
+    - y: flattened tensor of shape (B*H*W)
+    Returns
+    -------
+    - output: tensor of shape (B, C, H, W)
     """
-    shape = img.shape(x)
+    shape = x.shape
     batch_size = shape[0]
     height = shape[1]
     width = shape[2]
 
+    batch_idx = torch.arange(0, batch_size).unsqueeze(1).unsqueeze(1)
+    batch_idx = batch_idx.expand(batch_idx, height, width)
     
+    indices = torch.stack([batch_idx, y, x], dim=3)
+
+    return torch.gather(img, 2, indices)
